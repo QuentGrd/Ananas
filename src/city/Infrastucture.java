9908c2d69ec7b331d2098d16c3071a8c4c8578c5
MattVil <mattvilain@gmail.com>
2017-01-19 20:36:28 +0100

    Creation of the Infrastruture Class

--- src/city/Infrastucture.java
+++ src/city/Infrastucture.java
@@ -1,9 +1,14 @@
 package city;
 
+/**
+ * 
+ * @author Matthieu
+ *
+ */
 public abstract class Infrastucture {
 	
 	private String name;
-	private String address; //faire une classe coordonnées pour que l'adresse soit de type coordonnées
+	private String address; //faire une classe coordonnées pour que l'adresse soit de type coordonnées.
 	private int[] size;
 	private int nbUser;
 	
