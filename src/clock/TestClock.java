ec4e9ebc0be3fa68438008dcbf27f12a17e9a5f6
MattVil <mattvilain@gmail.com>
2017-01-22 12:22:59 +0100

    test of the new days/months/years features

--- src/clock/TestClock.java
+++ src/clock/TestClock.java
@@ -2,13 +2,13 @@ package clock;
  
 /**
  * This class is used to test the clock feature
- * @author quentin
- * @version 19012017
+ * @author quentin - matthieu
+ * @version 22012017
  */
 public class TestClock {
 
 	public static void main(String[] args) {
-		Clock c = new Clock(0, 0, 0);
+		Clock c = new Clock(0, 0, 0, 1, 1, 2017);
 		c.run();
 	}
 
