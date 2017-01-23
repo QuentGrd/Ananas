386fe19a2392bb71b07099e10476e63548101c3c
MattVil <mattvilain@gmail.com>
2017-01-23 23:10:13 +0100

    Test class creation - everything OK

---
+++ src/character/TestCharacter.java
@@ -0,0 +1,10 @@
+package character;
+
+public class TestCharacter {
+	public static void main(String[] args) {
+		for(int i=0; i<10; i++){
+			Character c = new Character();
+			System.out.println(c.toString());
+		}
+	}
+}
