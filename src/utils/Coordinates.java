ba497eb0f581c0164a977a8d2a587e7983821d14
QuentGrd <quentin.gerard2@gmail.com>
2017-01-23 17:03:56 +0100

    Add coordinates object

---
+++ src/utils/Coordinates.java
@@ -0,0 +1,33 @@
+package utils;
+
+public class Coordinates {
+	
+	private int x;
+	private int y;
+	
+	public Coordinates(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public String toString(){
+		return "x: " + this.x + " / y: " + this.y;
+	}
+	
+}
