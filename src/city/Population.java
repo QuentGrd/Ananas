7fe10d02a8f1c6e0367406ae7ccf353f742371a8
MattVil <mattvilain@gmail.com>
2017-01-25 10:57:24 +0100

    Population Class reOrganisation

--- src/city/Population.java
+++ src/city/Population.java
@@ -1,5 +1,36 @@
 package city;
 
+import java.util.ArrayList;
+
+import builders.PopulationBuilder;
+import character.Character;
+
 public class Population {
 
+	private int nbOfCharacter;
+	private ArrayList<Character> listCharacter;
+	
+	public Population(int nbOfCharacter){
+		this.nbOfCharacter = nbOfCharacter;
+		PopulationBuilder builder = new PopulationBuilder(this);
+	}
+
+	
+	
+	public int getNbOfCharacter() {
+		return nbOfCharacter;
+	}
+
+	public void setNbOfCharacter(int nbOfCharacter) {
+		this.nbOfCharacter = nbOfCharacter;
+	}
+
+	public ArrayList<Character> getListCharacter() {
+		return listCharacter;
+	}
+
+	public void setListCharacter(ArrayList<Character> listCharacter) {
+		this.listCharacter = listCharacter;
+	}
+	
 }
