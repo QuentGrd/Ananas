83fa31c3622445779a78c46c348e0bdc2ff3fe42
MattVil <mattvilain@gmail.com>
2017-01-25 09:22:29 +0100

    fix pull error

--- src/character/TestCharacter.java
+++ src/character/TestCharacter.java
@@ -2,9 +2,10 @@ package character;
 
 public class TestCharacter {
 	public static void main(String[] args) {
-		for(int i=0; i<10; i++){
+		for(int i=0; i<20; i++){
 			Character c = new Character();
 			System.out.println(c.toString());
+			System.out.println("lol");
 		}
 	}
 }
