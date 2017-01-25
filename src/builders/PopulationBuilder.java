80729dd9c78440d45a4336a5768fe1a9f54bd4e0
MattVil <mattvilain@gmail.com>
2017-01-25 10:18:19 +0100

    PopulationBuilder Class creation

---
+++ src/builders/PopulationBuilder.java
@@ -0,0 +1,20 @@
+package builders;
+
+import city.Population;
+
+/**
+ * 
+ * @author matthieu
+ * @version 25012017
+ */
+public class PopulationBuilder {
+
+	private Population pop;
+	
+	
+	public PopulationBuilder(Population pop){
+		this.pop = pop;
+	}
+	
+	
+}
