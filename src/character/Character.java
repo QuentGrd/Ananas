3cb29abed02894405fc321381c0649e20f111b16
MattVil <mattvilain@gmail.com>
2017-01-25 10:56:53 +0100

    Character Class reogrganisation

--- src/character/Character.java
+++ src/character/Character.java
@@ -1,14 +1,6 @@
 package character;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-import java.util.Random;
-
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
-
+import building.Home;
 import utils.BoundedCounter;
 
 /**
@@ -22,121 +14,44 @@ public class Character {
 	private int age;
 	private String name;
 	private String firstName;
-	//private Coordinates address;
+	private Home address;
 	
 	private BoundedCounter emotion;
 	
 	public Character(){
-		initCharacter();
-		age = randomSelection(1, 100);
-		emotion = new BoundedCounter(75, 0, 100);
-	}
-	/**
-	 * character's data initialization
-	 */
-	public void initCharacter(){
-		initName();
-		initFirstName();
-		initWork();
+
 	}
 	
-	/**
-	 * random name initialization by reading name.csv
-	 */
-	public void initName(){
-		String[] FILE_HEADER_MAPPING = {"name"};
-		
-		try{
-			CSVFormat csvFileFormat = CSVFormat.DEFAULT.withHeader(FILE_HEADER_MAPPING);
-			
-			FileReader fileReader = new FileReader("./res/name.csv");
-			CSVParser csvFileParser = new CSVParser(fileReader, csvFileFormat);
-			
-			List<CSVRecord> csvRecords = csvFileParser.getRecords();
-			
-			//random selection of name, first name and gender
-			int randomLineForName = randomSelection(0, csvRecords.size()-1);
-			
-			//research in file
-			for(int i=0; i<=randomLineForName; i++){
-				CSVRecord record = csvRecords.get(i);
-				if(i==randomLineForName){
-					this.name = record.get("name");
-				}
-			}
-			
-			
-			
-			csvFileParser.close();
-			fileReader.close();
-			
-		}catch(IOException e){
-			e.printStackTrace();
-		}
+	public boolean isGender() {
+		return gender;
 	}
-	
-	/**
-	 * random firstName and gender initialization by ready firstName.csv
-	 */
-	public void initFirstName(){
-		String[] FILE_HEADER_MAPPING = {"gender","firstName"};
-		
-		try{
-			CSVFormat csvFileFormat = CSVFormat.DEFAULT.withHeader(FILE_HEADER_MAPPING);
-			
-			FileReader fileReader = new FileReader("./res/firstName.csv");
-			CSVParser csvFileParser = new CSVParser(fileReader, csvFileFormat);
-			
-			List<CSVRecord> csvRecords = csvFileParser.getRecords();
-			
-			//random selection of name, first name and gender
-			int randomLineForFirstName = randomSelection(0, csvRecords.size()-1);
-			
-			//research in file
-			for(int i=0; i<=randomLineForFirstName; i++){
-				CSVRecord record = csvRecords.get(i);
-				if(i==randomLineForFirstName){
-					this.firstName = record.get("firstName");
-					String boolGender = record.get("gender");
-					if(boolGender.equals("1"))
-						this.gender = true;
-					if(boolGender.equals("0"))
-						this.gender = false;
-				}
-			}
-			
-			csvFileParser.close();
-			fileReader.close();
-			
-		}catch(IOException e){
-			e.printStackTrace();
-		}
+	public void setGender(boolean gender) {
+		this.gender = gender;
 	}
-	
-	public void initAddress(){
-		
+	public int getAge() {
+		return age;
 	}
-	
-	public void initWork(){
-		
+	public void setAge(int age) {
+		this.age = age;
 	}
-	
-	
-	/**
-	 * found a random number betwin min and max
-	 * @param min
-	 * @param max
-	 * @return random number found
-	 */
-	public int randomSelection(int min, int max){
-		int random;
-		Random rand = new Random();
-		
-		random = rand.nextInt(max - min +1) + min;
-		
-		return random;
+	public String getName() {
+		return name;
 	}
-	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public BoundedCounter getEmotion() {
+		return emotion;
+	}
+	public void setEmotion(BoundedCounter emotion) {
+		this.emotion = emotion;
+	}
 	public String toString(){
 		String str = "name : " + name + "\tfirstName : " + firstName + "\tage : " + age;
 		if(gender == true)
