b074497d11f006ce756c2cbf0aae0fd9244bda5b
MattVil <mattvilain@gmail.com>
2017-02-08 10:33:03 +0100

    console cleaning

--- src/city/TestCity.java
+++ src/city/TestCity.java
@@ -8,9 +8,6 @@ public class TestCity {
 	public static void main(String[] args) {
 		City c = new City();
 		CityBuilder cityB = new CityBuilder(c);
-		System.out.println(c.getMap().toString());
-		System.out.println(c.getMap().getInfrastructure(20, 14).toString());
-		System.out.println(c.getMap().getInfrastructure(6, 25).toString());
 		
 		GUIMain gui = new GUIMain(c.getMap());
 	}
