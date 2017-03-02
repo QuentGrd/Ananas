cbeb2ab3b0bb4bf93c547da8a954323fd86213e8
QuentGrd <quentin.gerard2@gmail.com>
2017-03-02 11:16:55 +0100

    Update GUI

--- src/gui/GUIInfoPart.java
+++ src/gui/GUIInfoPart.java
@@ -5,6 +5,8 @@ import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
@@ -24,6 +26,8 @@ import city.Population;
  */
 public class GUIInfoPart extends JPanel{
 
+	private static final long serialVersionUID = -560584172594140952L;
+
 	private Population pop;
 	
 	private JPanel buttonPart;
@@ -35,7 +39,7 @@ public class GUIInfoPart extends JPanel{
 	
 	private JPanel infoPart;
 	private JTextPane infoText;
-	private String textDefault = "First Name:\nName:\nID:\nEmotion\nHome:\nWork:\n";
+	private String textDefault = "First Name:\nName:\nID:\nEmotion:\nHome:\nWork:\n";
 	
 	private JPanel characterListPanel;
 	private static final String INFOPANEL = "Information Panel";
@@ -50,7 +54,6 @@ public class GUIInfoPart extends JPanel{
 		this.initCardLayout();
 		this.setPreferredSize(new Dimension(400, 600));
 		this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-		this.add(buttonPart);
 		this.add(cardsContainer);
 	}
 	
@@ -68,12 +71,12 @@ public class GUIInfoPart extends JPanel{
 	public void initButtonPart(){
 		buttonPart = new JPanel();
 		info = new JButton("_info");
-		list = new JButton("_list");
+		list = new JButton("Back");
 		info.addActionListener(new DisplayInfoAction());
 		list.addActionListener(new DisplayListAction());
 		buttonPart.setLayout(new FlowLayout());
 		buttonPart.add(list);
-		buttonPart.add(info);
+		//buttonPart.add(info);
 	}
 	
 	public void initInfoPanel(){
@@ -83,6 +86,7 @@ public class GUIInfoPart extends JPanel{
 		infoText.setText(textDefault);
 		infoText.setPreferredSize(new Dimension(390, 550));
 		infoPart.add(infoText);
+		infoPart.add(buttonPart);
 	}
 	
 	public void initCharacterListPanel(){
@@ -96,9 +100,54 @@ public class GUIInfoPart extends JPanel{
 		characterList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		characterList.setLayoutOrientation(JList.VERTICAL);
 		characterList.setVisibleRowCount(-1);
+		characterList.addMouseListener(new MouseListListener());
 		listScroller = new JScrollPane(characterList);
-		listScroller.setPreferredSize(new Dimension(390, 550));
+		listScroller.setPreferredSize(new Dimension(390, 590));
 		characterListPanel.add(listScroller);
+	}
+	
+	public String getCharacInfo(character.Character c){
+		String info = null;
+		if (c == null)
+			info = "This character is dead !";
+		else{
+			info = "First Name: \t" + c.getFirstName();
+			info += "\nName: \t" + c.getName();
+			info += "\nID: \t" + c.getId();
+			info += "\nEmotion: \t" + c.getEmotion().getCounter();
+			info += "\nHome: \t" + c.getHome().getAddress().toString();
+			info += "\nWork: \t" + c.getWork().getAddress().toString();
+		}
+		return info;
+	}
+	
+	class MouseListListener implements MouseListener{
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			if (e.getClickCount() == 2){
+				int i = characterList.getSelectedIndex();
+				character.Character charac = pop.getListCharacter().get(i);
+				infoText.setText(getCharacInfo(charac));
+				cl.show(cardsContainer, INFOPANEL);
+			}
+		}
+
+		@Override
+		public void mousePressed(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent e) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent e) {
+		}
+		
 	}
 	
 	class DisplayInfoAction implements ActionListener{
