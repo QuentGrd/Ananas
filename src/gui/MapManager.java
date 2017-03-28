7fcaf443a954280f90ab4a03cd63902b8a0bbd35
QuentGrd <quentin.gerard2@gmail.com>
2017-03-28 16:20:08 +0200

    Graphics Optimisation

--- src/gui/MapManager.java
+++ src/gui/MapManager.java
@@ -22,67 +22,56 @@ import utils.Coordinates;
 
 public class MapManager {
 	
+	private Image imgHome1;
+	private Image imgWork1;
+	private Image imgWork2;
+	private Image imgEnter1;
+	private Image imgRoadh;
+	private Image imgRoadw;
+	private Image player1;
+	
 	public MapManager(){
+		try {
+			imgHome1 = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/home" + 1 + ".png"));
+			imgWork1 = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/work0.png"));
+			imgWork2 = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/work.png"));
+			imgEnter1 = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/enter.png"));
+			imgRoadh = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/roadh.png"));
+			imgRoadw = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/roadw.png"));
+			player1 = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/player1.png")).getScaledInstance((20), (20), Image.SCALE_DEFAULT);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public Image printHome(Home home){
 		Coordinates size = home.getSize();
-		Image img = null;
-		try {
-			img = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/home" + 1 + ".png")).getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return img;
+		return imgHome1.getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
 	}
 	
 	public Image printWork(Work work){
 		Coordinates size = work.getSize();
-		Image img = null;
-		try{
-			if (size.getX() == size.getY())
-				img = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/work0.png")).getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
-			else
-				img = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/work.png")).getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
-		} catch(IOException e){
-			e.printStackTrace();
-		}
-		return img;
+		if (size.getX() == size.getY())
+			return imgWork1.getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
+		else
+			return imgWork2.getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
 	}
 	
 	public Image printEntertainment(Entertainment enter){
 		Coordinates size = enter.getSize();
-		Image img = null;
-		try {
-			img = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/enter.png")).getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return img;
+		return imgEnter1.getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
 	}
 	
 	public Image printRoad(Road road){
 		Coordinates size = road.getSize();
-		Image img = null;
-		try{
-			if(size.getX() > size.getY())
-				img = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/roadh.png")).getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
-			else
-				img = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/roadw.png")).getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
-		} catch(IOException e){
-			e.printStackTrace();
-		}
-		return img;
+		if(size.getX() > size.getY())
+			return imgRoadh.getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
+		else
+			return imgRoadw.getScaledInstance((size.getY()*20), (size.getX()*20), Image.SCALE_DEFAULT);
 	}
 	
 	public Image printCharacter(Character c){
-		Image img = null;
-		try {
-			img = ImageIO.read(new File(System.getProperty("user.dir") + "/res/img/player1.png")).getScaledInstance((20), (20), Image.SCALE_DEFAULT);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return img;
+		return player1;
 	}
 	
 }
