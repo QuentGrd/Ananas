dca30e711858d34d2bf12e5775a45fdb6bee078e
MattVil <mattvilain@gmail.com>
2017-04-06 13:24:54 +0200

    new methodes

--- src/autoMode/QPopulation.java
+++ src/autoMode/QPopulation.java
@@ -2,6 +2,8 @@ package autoMode;
 
 import java.util.ArrayList;
 
+import character.Character;
+
 /**
  * 
  * @author matthieu
@@ -27,9 +29,37 @@ public class QPopulation {
 
 	public ArrayList<QCharacter> getListCharacter() {
 		return listCharacter;
+	}	public Character[] getCharacterTab(){
+		int i;
+		Character charList[] = new Character[listCharacter.size()];
+		for (i=0; i<listCharacter.size(); i++){
+			charList[i] = listCharacter.get(i);
+		}
+		return charList;
+	}
+	
+	public String[] transform(){
+		Character[] list = this.getCharacterTab();
+		String nameList[];
+		nameList = new String[list.length];
+		int i;
+		for (i=0; i<list.length; i++){
+			nameList[i] = list[i].getFirstName() + " " + list[i].getName();
+		}
+		return nameList;
 	}
 
 	public void setListCharacter(ArrayList<QCharacter> listCharacter) {
 		this.listCharacter = listCharacter;
+	}
+	
+	public String toString(){
+		String str = "";
+		
+		for(int i=0; i<nbOfCharacter; i++){
+			System.out.println(listCharacter.get(i).toString());
+		}
+		
+		return str;
 	}
 }
