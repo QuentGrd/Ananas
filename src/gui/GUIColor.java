48e29b5ab4bc6959822073403581ae9c2e220f5f
QuentGrd <quentin.gerard2@gmail.com>
2017-04-26 13:20:07 +0200

    Fix

---
+++ src/gui/GUIColor.java
@@ -0,0 +1,16 @@
+package gui;
+
+import java.awt.Color;
+
+public class GUIColor {
+	
+	public static Color emotion = new Color(231, 76, 60);
+
+	public static Color family = new Color(26, 175, 93);
+	
+	public static Color money = new Color(52, 152, 219);
+	
+	public static Color background = new Color(236, 240, 241);
+	public static Color background1 = new Color(137, 196, 244);
+	public static Color background2 = new Color(139, 203, 222);
+}
